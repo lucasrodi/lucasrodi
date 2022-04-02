d42b3fba2581a1742a71cb708e1a45deb82c93ca
Lucas <luthasrod@gmail.com>
2022-04-02 09:55:12 -0300

    -_-

--- alura-challenge/web-app/src/Api/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: 'http://localhost:3000'
-})
-export const busca = async (url, setDado) => {
-    const resposta = await api.get(url)
-    setDado(resposta.data)
-}
